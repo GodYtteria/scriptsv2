eb91f7631ada622f3f171482b4694937f1a07a2c
GodYtteria <132059162+GodYtteria@users.noreply.github.com>
2024-04-03 10:37:16 +0800

    Create process_files.sh

---
+++ process_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /home/rdpuser/scripts/scriptsv2-main/myenv/bin/activate
+
+# Change directory to the script location
+cd /home/rdpuser/scripts/scriptsv2-main/
+
+# Run the first Python script and then run the second Python script if the first one succeeds,
+# then run the third Python script if the second one succeeds
+python /home/rdpuser/scripts/scriptsv2-main/new_fetchv2.py && \
+python /home/rdpuser/scripts/scriptsv2-main/new_calculationsv3.py && \
+python /home/rdpuser/scripts/scriptsv2-main/app.py
